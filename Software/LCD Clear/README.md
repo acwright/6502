2a9fdf305e6ac3580fa48dba23a69ed21ac98769
Aaron Wright <aaron.c.wright@icloud.com>
2024-07-03 03:18:12 -0500

    Add LCD Clear example

---
+++ Software/LCD Clear/README.md
@@ -0,0 +1,20 @@
+LCD Clear
+=========
+
+Simply clears the screen on the LCD Card.
+
+Install:
+
+    brew install cc65
+
+Build:
+
+    make
+
+View:
+
+    make view
+
+Burn EEPROM:
+
+    make eeprom
---
+++ Software/LCD Clear/src/LCD Clear.asm
@@ -0,0 +1,71 @@
+.setcpu "65C02"
+.segment "CODE"
+
+PORTB   = $8000
+PORTA   = $8001
+DDRB    = $8002
+DDRA    = $8003
+
+E       = %10000000
+RW      = %01000000
+RS      = %00100000
+
+reset:
+  ldx #$ff
+  txs
+
+  lda #%11111111 ; Set all pins on port B to output
+  sta DDRB
+  lda #%11100000 ; Set top 3 pins on port A to output
+  sta DDRA
+
+  lda #%00111000 ; Set 8-bit mode; 2-line display; 5x8 font
+  jsr lcd_instruction
+  lda #%00001100 ; Display on; cursor off; blink off
+  jsr lcd_instruction
+  lda #%00000110 ; Increment and shift cursor; don't shift display
+  jsr lcd_instruction
+
+clear:
+  lda #$00000001 ; Clear display
+  jsr lcd_instruction
+
+halt:
+  jmp halt
+
+lcd_wait:
+  pha
+  lda #%00000000  ; Port B is input
+  sta DDRB
+lcd_busy:
+  lda #RW
+  sta PORTA
+  lda #(RW | E)
+  sta PORTA
+  lda PORTB
+  and #%10000000
+  bne lcd_busy
+
+  lda #RW
+  sta PORTA
+  lda #%11111111  ; Port B is output
+  sta DDRB
+  pla
+  rts
+
+lcd_instruction:
+  jsr lcd_wait
+  sta PORTB
+  lda #0         ; Clear RS/RW/E bits
+  sta PORTA
+  lda #E         ; Set E bit to send instruction
+  sta PORTA
+  lda #0         ; Clear RS/RW/E bits
+  sta PORTA
+  rts
+
+.segment "VECTORS"
+
+  .word $0000
+  .word reset
+  .word $0000
