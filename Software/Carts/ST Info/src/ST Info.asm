35154e6ee15746a20ec5ae3c5b4494ec2c7aac8b
Aaron Wright <aaron.c.wright@icloud.com>
2025-11-05 08:15:58 -0600

    Try to read sector from CF

--- Software/Carts/ST Info/src/ST Info.asm
+++ Software/Carts/ST Info/src/ST Info.asm
@@ -48,6 +48,8 @@ reset:
 
   jsr cf_init
   jsr cf_info
+  jsr cf_init_sector
+  jsr cf_read_sector
 
   ldx #0
   jsr print
@@ -56,60 +58,102 @@ loop:
   jmp loop
 
 cf_init:
-  jsr cf_wait
-  lda #$04        ; Reset
+  jsr cf_wait_rdy
+  lda #$04                  ; Reset command
   sta CF_CMD
-  jsr cf_wait
+  jsr cf_wait_rdy
   lda #$00
   sta CF_LBA0
+  jsr cf_wait_bsy
   sta CF_LBA1
+  jsr cf_wait_bsy
   sta CF_LBA2
-  lda #$E0        ; LBA3=0, Master, Mode=LBA
+  jsr cf_wait_bsy
+  lda #$E0                  ; LBA3=0, Master, Mode=LBA
   sta CF_LBA3
-  lda #$01        ; 8-bit transfers
+  jsr cf_wait_bsy
+  lda #$01                  ; 8-bit transfers
   sta CF_FEAT
-  lda #$EF        ; Set feature
+  jsr cf_wait_bsy
+  lda #$EF                  ; Set feature
   sta CF_CMD
-  jsr cf_wait
+  jsr cf_wait_rdy
   jsr cf_err
   rts
 
 cf_info:
-  lda #$EC        ; Drive ID command
+  lda #$EC                  ; Drive ID command
   sta CF_CMD
-  jsr cf_wait
+  jsr cf_wait_rdy
   jsr cf_err
   ldy #0
 cf_info_loop:
-  lda CF_CMD
-  and #%00001000  ; DRQ flag
-  cmp #%00001000
-  bne cf_info_exit
+  jsr cf_wait_dat
   lda CF_DATA
   sta (CF_INFO_PTR),y
   iny
-  jsr cf_wait
-  jsr cf_err
-  jmp cf_info_loop
+  bne cf_info_loop
 cf_info_exit:
   rts
 
-cf_wait:
+cf_init_sector:
+  jsr cf_wait_rdy
+  lda #1                    ; Read 1 sector
+  sta CF_SECT
+  jsr cf_wait_bsy
+  lda #$20                  ; Read sector command
+  sta CF_CMD
+  jsr cf_wait_rdy
+  jsr cf_err
+  rts
+
+cf_read_sector:
+  ldy #0
+  ldx #2                    ; Read 2 pages (512 bytes)
+cf_read_sector_loop:
+  jsr cf_wait_dat
+  lda CF_DATA
+  sta (CF_BUFFER_PTR),y
+  iny
+  bne cf_read_sector_loop
+  inc CF_BUFFER_PTR + 1     ; Increment high byte of pointer
+  dex
+  bne cf_read_sector_loop
+  dec CF_BUFFER_PTR + 1     ; Restore original pointer
+  dec CF_BUFFER_PTR + 1
+cf_read_sector_exit:
+  rts
+
+cf_wait_bsy:
   lda CF_STAT
-  and #%11000000  ; Mask out BSY and RDY flags
+  and #%10000000
+  bne cf_wait_bsy
+cf_wait_bsy_exit:
+  rts
+
+cf_wait_rdy:
+  lda CF_STAT
+  and #%11000000            ; Mask out BSY and RDY flags
   cmp #%01000000
-  bne cf_wait
-cf_wait_exit:
+  bne cf_wait_rdy
+cf_wait_rdy_exit:
+  rts
+
+cf_wait_dat:
+  lda CF_STAT
+  and #%11001000            ; Mask out BSY, RDY and DRQ flags
+  cmp #%01001000
+  bne cf_wait_dat
+cf_wait_dat_exit:
   rts
 
 cf_err:
-  jsr cf_wait
   lda CF_STAT
-  and #$01            ; Mask out error flag
+  and #%00000001            ; Mask out error flag
   beq cf_no_err
   lda #'!'
   jsr acia_send_char
-  lda CF_ERR          ; Get error code
+  lda CF_ERR                ; Get error code
   jsr acia_send_char
 cf_no_err:
   rts
@@ -121,18 +165,18 @@ print:
   inx
   jmp print
 print_crlf:
-  lda #$0D        ; Carriage Return
+  lda #$0D                   ; Carriage Return
   jsr acia_send_char
-  lda #$0A        ; Line feed
+  lda #$0A                   ; Line feed
   jsr acia_send_char
   rts
 
 acia_init:
   lda #$00
-  sta ACIA_STATUS ; Soft reset (value not important)
-  lda #%00011111  ; N-8-1, 19200 baud
+  sta ACIA_STATUS             ; Soft reset (value not important)
+  lda #%00011111              ; N-8-1, 19200 baud
   sta ACIA_CTRL
-  lda #%00001011  ; No parity, No echo, No interrupts
+  lda #%00001011              ; No parity, No echo, No interrupts
   sta ACIA_CMD
   rts
 
@@ -141,8 +185,8 @@ acia_send_char:
   pha
 acia_tx_wait:
   lda ACIA_STATUS
-  and #%00010000  ; Check if tx buffer not empty
-  beq acia_tx_wait     ; Loop if tx buffer not empty
+  and #%00010000              ; Check if tx buffer not empty
+  beq acia_tx_wait            ; Loop if tx buffer not empty
   pla
   rts
 
