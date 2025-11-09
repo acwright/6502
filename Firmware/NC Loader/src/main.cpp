772d390e891b779d9fa8b08f97a9e800b2526301
Aaron Wright <aaron.c.wright@icloud.com>
2025-11-08 19:54:40 -0600

    Working on NC Loader

--- Firmware/NC Loader/src/main.cpp
+++ Firmware/NC Loader/src/main.cpp
@@ -1,26 +1,38 @@
 #include <Arduino.h>
+#include <XModem.h>
 #include "constants.h"
 #include "pins.h"
 
 void onCommand(char command);
+bool receiveData(void *blk_id, size_t idSize, byte *data, size_t dataSize);
 
 void info();
 void reset();
-void load();
+void halt();
+void resume();
 
 void initPins();
 
 void writeAddress(uint16_t address);
 void writeData(uint8_t data);
 
+bool isLoading = false;
+bool isHalted = false;
+
+XModem xmodem;
+
+uint8_t receivedData[128];
+
 //
 // MAIN
 //
 
 void setup() {
-  Serial.begin(115200);
-
+  Serial.begin(9600);
   while (!Serial);
+
+  xmodem.begin(Serial, XModem::ProtocolType::XMODEM);
+  xmodem.setRecieveBlockHandler(receiveData);
   
   initPins();
 
@@ -28,7 +40,19 @@ void setup() {
 }
 
 void loop() {
-  if (Serial.available()) {
+  if (isLoading) {
+    //if (!isHalted) { halt(); }
+    Serial.print("Waiting for data..");
+    xmodem.receive();
+    Serial.println();
+    for (unsigned int i = 0; i < 128; i++) {
+      Serial.print(receivedData[i], HEX);
+      Serial.print("");
+    }
+    Serial.println();
+    //if (isHalted) { resume(); }
+    isLoading = false;
+  } else if (Serial.available()) {
     onCommand(Serial.read());
   }
 }
@@ -47,11 +71,21 @@ void onCommand(char command) {
     case 'T':
       reset();
       break;
-    case 'u':
-    case 'U':
-      load();
+    case 'l':
+    case 'L':
+      isLoading = true;
       break;
   }
+}
+
+bool receiveData(void *blk_id, size_t idSize, byte *data, size_t dataSize) {
+  // byte id = *((byte *) blk_id);
+
+  for(unsigned int i = 0; i < dataSize; ++i) {
+    receivedData[i] = data[i];
+  }
+
+  return true; // or return false to stop transfer early
 }
 
 //
@@ -60,12 +94,12 @@ void onCommand(char command) {
 
 void info() {
   Serial.println();
-  Serial.println("eeee  eeeee eeeeee eeee   88888 888888   8                                    ");                        
-  Serial.println("8  8  8     8    8    8   8   8 8    e   8     eeeee eeeee eeeee eeee eeeee   ");
-  Serial.println("8     8eeee 8    8    8   8e  8 8e       8e    8  88 8   8 8   8 8    8   8   ");
-  Serial.println("8eeee     8 8    8 eee8   88  8 88       88    8   8 8eee8 8e  8 8eee 8eee8e  ");
-  Serial.println("8   8     8 8    8 8      88  8 88   e   88    8   8 88  8 88  8 88   88   8  ");
-  Serial.println("8eee8 eeee8 8eeee8 8eee   88  8 88eee8   88eee 8eee8 88  8 88ee8 88ee 88   8 ");
+  Serial.println("eeee  eeeee eeeeee eeee   88888 888888   8                                  ");                        
+  Serial.println("8  8  8     8    8    8   8   8 8    e   8     eeeee eeeee eeeee eeee eeeee ");
+  Serial.println("8     8eeee 8    8    8   8e  8 8e       8e    8  88 8   8 8   8 8    8   8 ");
+  Serial.println("8eeee     8 8    8 eee8   88  8 88       88    8   8 8eee8 8e  8 8eee 8eee8e");
+  Serial.println("8   8     8 8    8 8      88  8 88   e   88    8   8 88  8 88  8 88   88   8");
+  Serial.println("8eee8 eeee8 8eeee8 8eee   88  8 88eee8   88eee 8eee8 88  8 88ee8 88ee 88   8");
   Serial.println();
   Serial.println("6502 NC Loader | Version: 1.0");
   Serial.println();
@@ -92,8 +126,42 @@ void reset() {
   Serial.println("Done");
 }
 
-void load() {
-  
+void halt() {
+  Serial.print("Halting the 6502... ");
+
+  while (!digitalRead(SYNC));
+
+  pinMode(RDY, OUTPUT);
+  digitalWrite(RDY, LOW);
+
+  Serial.println("Done");
+
+  Serial.print("Disabling the bus... ");
+
+  pinMode(BE, OUTPUT);
+  digitalWrite(BE, LOW);
+
+  isHalted = true;
+
+  Serial.println("Done");
+}
+
+void resume() {
+  Serial.print("Re-enabling the bus... ");
+
+  digitalWrite(BE, HIGH);
+  pinMode(BE, INPUT);
+
+  Serial.println("Done");
+
+  Serial.print("Resuming the 6502... ");
+
+  digitalWrite(RDY, HIGH);
+  pinMode(RDY, INPUT);
+
+  isHalted = false;
+
+  Serial.println("Done");
 }
 
 //
