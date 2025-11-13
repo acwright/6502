9bc7f309c34a1c46138eb5c240a99a86e7c749ba
Aaron Wright <aaron.c.wright@icloud.com>
2025-11-13 15:25:31 -0600

    Remove JS Emulator for now

--- Emulator/README.md
+++
@@ -1,4 +0,0 @@
-6502 Emulator
-============= 
-
-A Node.js-based emulator for the 6502 microprocessor.
--- Emulator/src/components/IO/VGACard.ts
+++
@@ -1,8 +0,0 @@
-import { IO, IODescription } from '../IO'
-import { VideoCard } from './VideoCard'
-
-export class VGACard extends VideoCard implements IO {
-
-  static DESCRIPTION: IODescription = { className: 'VGACard', title: 'VGA Card' }
-
-}
