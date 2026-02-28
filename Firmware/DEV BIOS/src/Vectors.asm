e015b2d3873dbb77f972b6d5e7c9815f5cd52814
Aaron Wright <aaron.c.wright@icloud.com>
2026-02-27 21:57:39 -0600

    Remove BIOS to 6502-ASM project

--- Firmware/DEV BIOS/src/Vectors.asm
+++
@@ -1,7 +0,0 @@
-; ***             ***
-; ***   VECTORS   ***
-; ***             ***
-
-.word   NMI_VEC           ; NMI vector
-.word   RESET_VEC         ; RESET vector
-.word   IRQ_VEC           ; IRQ vector
